3f6f260bf5533698e3c337d81cd1a0809e5ed370
rafaelramos64 <rafaelmaggot64@gmail.com>
2021-10-22 14:30:22 -0300

    adjusts

--- src/views/ListWallets.vue
+++ src/views/ListWallets.vue
@@ -5,7 +5,10 @@
         <v-col cols="12" md="8">
           <v-card elevation="6"
             class="card-border">
-            <v-card-title class="">Lista de Carteiras</v-card-title>
+            <v-card-title
+              class="justify-center mb-3 ">
+              <h1 style="color: #887725">Lista de Carteiras</h1>
+            </v-card-title>
 
             <v-simple-table
               fixed-header
@@ -13,17 +16,17 @@
               <template v-slot:default>
                 <thead>
                   <tr>
-                    <th class="text-left">
-                      Nome
+                    <th class="text-left title-color">
+                      <h2 class="-color">Nome</h2>
                     </th>
-                    <th class="text-left">
-                      Carteira
+                    <th class="text-left title-color">
+                      <h2 class="-color">Carteira</h2>
                     </th>
-                    <th class="text-left">
-                      Saldo
+                    <th class="text-left title-color">
+                      <h2 class="-color">Saldo</h2>
                     </th>
-                    <th class="text-center">
-                      Actions
+                    <th class="text-center title-color">
+                      <h2 class="-color">Ações</h2>
                     </th>
                   </tr>
                 </thead>
@@ -38,35 +41,49 @@
                     <td class="text-center">
                       <v-hover
                         v-slot="{ hover }">
-                        <v-btn
-                          x-small
-                          :elevation="hover ? 16 : 0"
-                          outlined
-                          :class="hover ? 'change-btn-color' : ''"
-                          dark
-                          color="#887725"
-                          class="mr-4 justify-center py-3 px-1"
-                          @click="setBalance('add')"
-                        >
-                          <v-icon>mdi-plus</v-icon>
-                          Saldo
-                        </v-btn>
+                        <v-tooltip top color="#887725">
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                              v-bind="attrs"
+                              v-on="on"
+                              x-small
+                              :elevation="hover ? 16 : 0"
+                              outlined
+                              :class="hover ? 'change-btn-color' : ''"
+                              dark
+                              color="#887725"
+                              class="mr-4 justify-center py-3 px-1"
+                              @click="setBalance('add')"
+                            >
+                              <v-icon>mdi-plus</v-icon>
+                              Saldo
+                            </v-btn>
+                          </template>
+                          <span>Adicionar Créditos</span>
+                        </v-tooltip>
                       </v-hover>
 
                       <v-hover
                         v-slot="{ hover }">
-                        <v-btn
-                          x-small
-                          :elevation="hover ? 16 : 0"
-                          :outlined="hover ? false : true"
-                          dark
-                          color="error"
-                          class="mr-4 justify-center py-3 px-1"
-                          @click="setBalance('minus')"
-                        >
-                          <v-icon>mdi-minus</v-icon>
-                          Saldo
-                        </v-btn>
+                        <v-tooltip top color="#887725">
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                              v-bind="attrs"
+                              v-on="on"
+                              x-small
+                              :elevation="hover ? 16 : 0"
+                              :outlined="hover ? false : true"
+                              dark
+                              color="error"
+                              class="mr-4 justify-center py-3 px-1"
+                              @click="setBalance('minus')"
+                            >
+                              <v-icon>mdi-minus</v-icon>
+                              Saldo
+                            </v-btn>
+                          </template>
+                          <span>Sacar</span>
+                        </v-tooltip>
                       </v-hover>
                     </td>
                   </tr>
@@ -147,4 +164,13 @@
   position: relative;
   bottom: 2px;
 }
+
+.-color {
+  color:#C3AB6A;
+}
+
+.btn-balance:hover {
+  box-shadow: 0 8px 10px -5px rgba(0,0,0,.2),0 16px 24px 2px rgba(0,0,0,.14),0 6px 30px 5px rgba(0,0,0,.12)!important;
+}
+
 </style>
