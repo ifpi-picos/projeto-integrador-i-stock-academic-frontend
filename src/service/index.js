5ae1671be75ee26e25dc88515c2b0b2a40959d57
Edmarques Lima <edmarquessantosdelima@gmail.com>
2023-04-21 20:58:53 -0300

    chore: change baseUrl from api

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import Vue from 'vue'
 
 const api = Axios.create({
-  baseURL: 'https://stock-academic-api.herokuapp.com/api'
+  baseURL: 'https://leaf-backend.onrender.com/api'
 })
 
 const api2 = Axios.create({
